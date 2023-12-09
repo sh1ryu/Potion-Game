727e1043055e80849dc66ba0d248cd4fd8fa0b39
shiryu <621nua@gmail.com>
2023-12-09 11:19:32 +0300

    update script

--- potion-game/Assets/Scripts/Destroyer.cs
+++ potion-game/Assets/Scripts/Destroyer.cs
@@ -2,6 +2,14 @@
 
 public class Destroyer : MonoBehaviour
 {
+    private void Update()
+    {
+        if (transform.position.y < -1)
+        {
+            Destroy(gameObject);
+        }
+    }
+
     private void OnCollisionEnter(Collision collision)
     {
         Destroy(gameObject);
